2aaf28b197f052378d71c8aec00026cd2a773078
Jason Bao <bao.jason.z@gmail.com>
2021-12-20 15:16:13 -0800

    Day 1 Part 2

--- day1/main.go
+++ day1/main.go
@@ -6,8 +6,7 @@ import (
 	"strconv"
 )
 
-func part1() {
-	input := req.FormattedRequest(1)
+func part1(input []string) {
 	lastVal, increaseCount := -1, -1
 	for _, val := range input {
 		valInt, err := strconv.Atoi(val)
@@ -19,9 +18,33 @@ func part1() {
 		}
 		lastVal = valInt
 	}
-	fmt.Println(increaseCount)
+	fmt.Println("Part 1: %v", increaseCount)
+}
+
+func part2(input []string) {
+	lastVal, increaseCount := -1, -1
+	for i, _ := range input {
+		if i > len(input)-3 {
+			break
+		}
+		sum := 0
+		for j := 0; j < 3; j++ {
+			valInt, err := strconv.Atoi(input[i+j])
+			if err != nil {
+				panic(err)
+			}
+			sum += valInt
+		}
+		if sum > lastVal {
+			increaseCount += 1
+		}
+		lastVal = sum
+	}
+	fmt.Println("Part 2: %v", increaseCount)
 }
 
 func main() {
-	part1()
+	input := req.FormattedRequest(1)
+	part1(input)
+	part2(input)
 }
