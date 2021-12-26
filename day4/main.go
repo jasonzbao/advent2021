df4e22aa8246a54c8ab4ab80431d83ca45ec0d60
Jason Bao <bao.jason.z@gmail.com>
2021-12-25 21:57:18 -0800

    day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -28,6 +28,31 @@ func NewBoard() Board {
 	}
 }
 
+func InitBoards(input []string) (boards []Board) {
+	for i, val := range input {
+		// first 2 lines are for commands
+		if i == 0 || i == 1 {
+			continue
+		}
+
+		shiftI := i - 2 // shift for commands
+		if shiftI%6 == 0 {
+			boards = append(boards, NewBoard())
+		}
+		if shiftI%6 != 5 {
+			splitI := 0
+			for _, val := range strings.Split(val, " ") {
+				if val == "" {
+					continue
+				}
+				boards[len(boards)-1].values[shiftI%6][splitI] = val
+				splitI += 1
+			}
+		}
+	}
+	return
+}
+
 func (b *Board) isComplete() bool {
 	for i := 0; i < 5; i++ {
 		complete1, complete2 := 1, 1
@@ -69,29 +94,7 @@ func (b *Board) sum() (sum int) {
 
 func part1(input []string) {
 	commands := strings.Split(input[0], ",")
-	boards := []Board{}
-	for i, val := range input {
-		// first 2 lines are for commands
-		if i == 0 || i == 1 {
-			continue
-		}
-
-		shiftI := i - 2 // shift for commands
-		if shiftI%6 == 0 {
-			boards = append(boards, NewBoard())
-		}
-		if shiftI%6 != 5 {
-			splitI := 0
-			for _, val := range strings.Split(val, " ") {
-				if val == "" {
-					continue
-				}
-				boards[len(boards)-1].values[shiftI%6][splitI] = val
-				splitI += 1
-			}
-		}
-	}
-
+	boards := InitBoards(input)
 	for _, command := range commands {
 		for _, board := range boards {
 			board.mark(command)
@@ -107,11 +110,42 @@ func part1(input []string) {
 			}
 		}
 	}
+}
+
+func remove(s []Board, i int) []Board {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func part2(input []string) {
+	commands := strings.Split(input[0], ",")
+	boards := InitBoards(input)
+	var lastWin int
+	for _, command := range commands {
+		for _, board := range boards {
+			board.mark(command)
+		}
+		var winners []int
+		for i, board := range boards {
+			if board.isComplete() {
+				lastNum, err := strconv.Atoi(command)
+				if err != nil {
+					panic(err)
+				}
+				lastWin = board.sum() * lastNum
+				winners = append(winners, i)
+			}
+		}
+		for i, _ := range winners {
+			boards = remove(boards, winners[len(winners)-1-i])
+		}
+	}
+	fmt.Printf("Part 2: %v", lastWin)
 
 }
 
 func main() {
 	input := req.FormattedRequest(4)
 	part1(input)
-	// part2(input)
+	part2(input)
 }
